57d3221deb037f8d18e0e33769483f13f8c01e89
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-04-07 09:37:17 +0200

    Initialised the main properties of PersonDetails class

---
+++ src/unoverriden/PersonDetails.java
@@ -0,0 +1,16 @@
+package unoverriden;
+
+/**
+ * PersonDetails is the class we'll be using to demonstrate comparable classes.
+ *
+ * @author SAUVE Jean-Luc
+ *
+ */
+public class PersonDetails {
+
+    private Integer age;
+    private String firstName;
+    private String lastName;
+    private String nationality;
+
+}
