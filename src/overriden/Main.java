09e8a678349290a745b936ee029731ff349da818
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-04-07 11:33:47 +0200

    Wrote main to test overriden PersonDetails

--- src/overriden/Main.java
+++ src/overriden/Main.java
@@ -1,4 +1,21 @@
 package overriden;
 
+/**
+ * Main class to compare instances of PersonDetails without overriding .
+ *
+ * @author SAUVE Jean-Luc
+ *
+ */
 public class Main {
+    public static void main(String[] args){
+       PersonDetails first = new PersonDetails(18,"John","Doe","American");
+       PersonDetails second = new PersonDetails(18,"John","Doe","American");
+
+        if(first.equals(second)){
+            System.out.println("Equal");
+        }
+        else {
+            System.out.println("Not equal");
+        }
+    }
 }
